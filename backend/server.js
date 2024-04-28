1292077623c6b9f110ad32df519b10b44457eae9
Durga0808 <bejawada.durgaprasad@gmail.com>
2024-04-28 14:37:57 +0530

    few modifications

--- backend/server.js
+++ backend/server.js
@@ -15,23 +15,20 @@ const pool = new Pool({
   port: 5432, //port number for postgresql
 });
 
-
 //creating api endponit
-app.get("/api/data", async (req,res) => {
+app.get("/data", async (req, res) => {
   try {
     const client = await pool.connect(); //connect to database
     const result = await client.query("SELECT * FROM sample_data"); //making query to get data
     client.release(); //connecion stopped.
     res.json(result.rows); //returning data
   } catch (err) {
-    console.error("Error executing query", err);//printing error
-    res.status(500).json({ error: "Internal Server Error" });//returning error status
+    console.error("Error executing query", err); //printing error
+    res.status(500).json({ error: "Internal Server Error" }); //returning error status
     console.log("in error");
   }
 });
 
-
-app.listen(PORT,() => {
-    console.log(`server running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
 });
-
